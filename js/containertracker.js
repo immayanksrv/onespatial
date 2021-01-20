96b1f2115f51b26de762c8998bab88e3558da04b
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2021-01-20 05:37:21 -0500

    change path

--- js/containertracker.js
+++ js/containertracker.js
@@ -265,7 +265,7 @@ function getAllGeofences() {
 function getAllMarkers() {
     var info_window = new google.maps.InfoWindow({ content: '' });
     $.ajax({
-        url: "/Container.txt",
+        url: "Container.txt",
         success: function (result) {
             var results = JSON.parse(result);
             var container_m;
